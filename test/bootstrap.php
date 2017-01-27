34af97a89beb2efbe2cc6ca63b991e4b7bf52b21
Thierry <Thierry@192.168.1.3>
2017-01-27 16:44:53 +0100

    List folder paths from where includes files will be extracted by autoload.php

---
+++ test/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+    include_once('AutoLoader.php');
+    // Register the directory to your include files
+    AutoLoader::registerDirectory('/Applications/Noalyss/apps/noalyss/htdocs/include/class');
+    AutoLoader::registerDirectory('/Applications/Noalyss/apps/noalyss/htdocs/include/database');
+    AutoLoader::registerDirectory('/Applications/Noalyss/apps/noalyss/htdocs/include');
