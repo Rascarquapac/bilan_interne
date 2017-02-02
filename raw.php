420dbb17e4e54575c4d08e86a74467b280b0d583
Thierry <Thierry@192.168.1.3>
2017-02-02 11:31:37 +0100

    Management of PDF,CSV exports according to Noalyss plugins export way

---
+++ raw.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ *   This file is part of NOALYSS.
+ *
+ *   NOALYSS is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   NOALYSS is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with NOALYSS; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+/* $Revision$ */
+
+/*!\file
+ * \brief raw file for PDF ewa
+ */
+require_once NOALYSS_INCLUDE.'/ext/bilan_interne/class_acc_bilaninterne.php';
+require_once NOALYSS_INCLUDE. '/ext/bilan_interne/class_output_bilaninterne.php';
+
+
+extract($_REQUEST);
+$cn=Dossier::connect();
+$bilaninterne=new Acc_Bilaninterne($cn);
+$bilaninterne->b_id =$b_id;
+$bilaninterne->from =$from_periode;
+$bilaninterne->to   =$to_periode;
+$bilaninterne->load();
+$bilaninterne->generate();
+$output = new output_bilaninterne;
+$output->from=$from_periode;
+$output->to  =$to_periode;
+if ($act == 'export_bilaninterne_csv')
+{
+    $output->output_csv($bilaninterne->bilan_table,$cn);
+    exit();
+}
+if ($act == 'export_bilaninterne_pdf')
+{
+    $output->output_pdf($bilaninterne->bilan_table,$cn);
+    exit();
+}
+if ($act == 'export_bilaninterne_print')
+{
+    $output->output_html($bilaninterne->bilan_table,$cn);
+    exit();
+}
