9ef451bb6f5b6098bef6633fefde58bb329af71e
Thierry <Thierry@192.168.1.3>
2017-02-05 18:12:29 +0100

    Adding constant BILAN_INTERNE_HOME to make "includes" independent of plugin path

---
+++ bilaninterne_constant.php
@@ -0,0 +1,17 @@
+<?php 
+//This file is part of NOALYSS and is under GPL 
+//see licence.txt
+/**
+ *@file
+ *Contains all the needed variable for the plugin
+ *is name is plugin_name_constant.php
+ * You can use some globale variable, especially for the database
+ *  connection
+ */
+
+if ( !defined("BILAN_INTERNE_HOME")) define ("BILAN_INTERNE_HOME",__DIR__);
+require_once NOALYSS_INCLUDE .'/lib/class_database.php';
+
+global $cn,$errcode;
+
+?>
